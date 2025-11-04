8025dd5939579a2e1de0b7b16321d2ccea96a04f
enricorizzi-cmd <enrico.rizzi@dimensioneagricoltura.it>
2025-11-04 01:40:26 +0100

    Fix: Cambiato return type di urlBase64ToUint8Array a BufferSource

--- lib/client-push.ts
+++ lib/client-push.ts
@@ -86,7 +86,7 @@ export async function subscribeToPushNotifications(): Promise<PushSubscription |
   }
 }
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): BufferSource {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
 
